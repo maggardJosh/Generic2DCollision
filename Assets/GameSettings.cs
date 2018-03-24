c4e756ace7cbbf4a9634646f4385d7e9d95cc5f1
Josh Maggard <jmaggard62@gmail.com>
2018-03-24 17:04:24 -0500

    x and y movement working

---
+++ Assets/GameSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+
+    private static GameSettings _instance;
+    public static GameSettings Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<GameSettings>();
+            return _instance;
+        }
+    }
+    public float tileHeight = 1;
+    public static float TileSize { get { return Instance.tileHeight; } }
+
+    public float collisionOffsetValue = .01f;
+    public static float CollisionOffsetValue { get { return Instance.collisionOffsetValue; } }
+
+    // Use this for initialization
+    void Start()
+    {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -31,24 +31,60 @@ public class PlayerController : MonoBehaviour
             Vector3 moveVect = Vector3.right;
             if (moveVelocity.x < 0)
                 moveVect = Vector3.left;
-            Vector3 rightCenter = bCollider.transform.position + bCollider.center + (moveVect * bCollider.size.x / 2.1f);
             Vector3 newPos = transform.position + new Vector3(moveVelocity.x, 0);
-            for (int i = 0; i < 2; i++)
+            float numSections = bCollider.size.y / GameSettings.TileSize + 1;
+            float offsetValue = GameSettings.CollisionOffsetValue;
+            Vector3 sideCenter = bCollider.transform.position + bCollider.center + moveVect * (bCollider.size.x / 2 - offsetValue);
+
+            for (int i = 0; i <= numSections; i++)
             {
                 Vector3 tempPos = transform.position + new Vector3(moveVelocity.x, 0);
-                //i==0 top right corner
-                //i==1 bottom right corner
-                Ray ray = new Ray(rightCenter + (i == 0 ? Vector3.up : Vector3.down) * bCollider.size.y / 2.1f, moveVect);
+                //i==0 top side corner
+                //i==1 bottom side corner
+                float ySectionValue = Mathf.Min(i * GameSettings.TileSize, bCollider.size.y - offsetValue * 2f);
+                Ray ray = new Ray(sideCenter + Vector3.up * (bCollider.size.y / 2 - offsetValue) + (Vector3.down * ySectionValue), moveVect);
                 if (Physics.Raycast(ray, out hitResult, Mathf.Abs(moveVelocity.x), CollideMask))
                 {
                     Debug.DrawLine(ray.origin, ray.origin + ray.direction * Mathf.Abs(moveVelocity.x), Color.red, 5f);
-                    tempPos = new Vector3(hitResult.point.x, tempPos.y) - moveVect * (bCollider.size.x / 2f);
+                    tempPos = new Vector3(hitResult.point.x - bCollider.center.x, tempPos.y) - moveVect * (bCollider.size.x / 2f);
                 }
                 else
                 {
                     Debug.DrawLine(ray.origin, ray.origin + ray.direction * Mathf.Abs(moveVelocity.x), Color.green, .5f);
                 }
-                Debug.Log((tempPos - transform.position).sqrMagnitude.ToString() + (newPos - transform.position).sqrMagnitude);
+                if ((tempPos - transform.position).sqrMagnitude < (newPos - transform.position).sqrMagnitude)
+                    newPos = tempPos;   //Basically we ran into something so use this temppos
+            }
+            transform.position = newPos;
+        }
+
+        //Try move up or down now
+        if (Mathf.Abs(moveVelocity.y) > 0)
+        {
+            Vector3 moveVect = Vector3.up;
+            if (moveVelocity.y < 0)
+                moveVect = Vector3.down;
+            Vector3 newPos = transform.position + new Vector3(0, moveVelocity.y);
+            float numSections = bCollider.size.x / GameSettings.TileSize + 1;
+            float offsetValue = GameSettings.CollisionOffsetValue;
+            Vector3 verticalCenter = bCollider.transform.position + bCollider.center + moveVect * (bCollider.size.y / 2 - offsetValue);
+
+            for (int i = 0; i <= numSections; i++)
+            {
+                Vector3 tempPos = transform.position + new Vector3(0, moveVelocity.y);
+                //i==0 left vertical corner
+                //i==1 right vertical corner
+                float xSectionValue = Mathf.Min(i * GameSettings.TileSize, bCollider.size.x - offsetValue * 2f);
+                Ray ray = new Ray(verticalCenter + Vector3.left * (bCollider.size.x / 2 - offsetValue) + (Vector3.right * xSectionValue), moveVect);
+                if (Physics.Raycast(ray, out hitResult, Mathf.Abs(moveVelocity.y), CollideMask))
+                {
+                    Debug.DrawLine(ray.origin, ray.origin + ray.direction * Mathf.Abs(moveVelocity.y), Color.red, 5f);
+                    tempPos = new Vector3(tempPos.x, hitResult.point.y - bCollider.center.y) - moveVect * (bCollider.size.y / 2f);
+                }
+                else
+                {
+                    Debug.DrawLine(ray.origin, ray.origin + ray.direction * Mathf.Abs(moveVelocity.y), Color.green, .5f);
+                }
                 if ((tempPos - transform.position).sqrMagnitude < (newPos - transform.position).sqrMagnitude)
                     newPos = tempPos;   //Basically we ran into something so use this temppos
             }
@@ -64,12 +100,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-            velocity = Vector3.left * speed;
-        else if (Input.GetKey(KeyCode.D))
-            velocity = Vector3.right * speed;
+        velocity = Vector3.zero;
 
-        else velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+            velocity += Vector3.left * speed;
+        else if (Input.GetKey(KeyCode.D))
+            velocity += Vector3.right * speed;
+
+
+        if (Input.GetKey(KeyCode.W))
+            velocity += Vector3.up * speed;
+        else if (Input.GetKey(KeyCode.S))
+            velocity += Vector3.down * speed;
+
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
     }
